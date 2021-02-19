352dc1a6b5bd5ee47bcfa7394d7b3ef8c8b3d2ef
creatorneemay <72016905+creatorneemay@users.noreply.github.com>
2021-02-19 19:34:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,5 +2,22 @@ function preload(){}
 function setup(){
     canvas=createCanvas(400,400);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    posenet=ml5.poseNet(video,modelloaded)
+    posenet.on("pose",getposes);
 }
-function draw(){}
+function draw(){
+    image(video,0,0,400,400)
+}
+function modelloaded(){
+    console.log("posenet model is loaded")
+}
+function getposes(results){
+    if(results.length>0){
+        console.log(results)
+        nosex=results[0].pose.nose.x
+        nosey=results[0].pose.nose.y
+        console.log(nosex,nosey)
+    }
+}
